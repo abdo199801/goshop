478237c16b41ef7ffa2ed72e15937f644a7cb210
Abdellah Baqba <abdobaq777@gmail.com>
2025-12-15 15:23:15 +0100

    misspelling

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -7,7 +7,7 @@ export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
-    syncUserCreation,
+    syncUserDeletion,
     syncUserUpdation,
     syncUserDeletion,
     
